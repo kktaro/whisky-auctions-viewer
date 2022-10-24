48a88f13f8bad1c8d41f59d9d6f4bc273a1a8520
Takuya Mutai <kk.nnkktr@gmail.com>
2022-10-24 20:45:35 +0900

    ルータを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whisky_auctions_viewer/router/app_router.dart';
 
+void main() {
+  runApp(const ProviderScope(child: AppRouter()));
+}
